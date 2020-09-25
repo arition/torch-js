62e6246d7a0df6962c3d0c103ce78d108fa41383
Raghav Mecheri <raghav.mecheri@columbia.edu>
2020-09-25 19:04:33 +0530

    Functional basic tests for the torch.rand function

--- tests/rand.test.js
+++ tests/rand.test.js
@@ -1,24 +1,47 @@
+/**
+ * Test module for Random tensor creation
+ * TODO: Check why a.shape is of type object, instead of type Array
+ * TODO: Implement toBeInstanceOf checks for data children of objects
+ */
 import {describe, expect, test} from '@jest/globals'
 
 const torch = require("../dist");
 
-const test_model_path = __dirname + "/resources/test_model.pt";
-const script_module = new torch.ScriptModule(test_model_path);
-
 describe('Random tensor creation', () => {
 	test('Random tensor creation using variable number of arguements', () => {
-		// TODO
-		expect(true).toBe(true);
+		const a = torch.rand(1, 5).toObject();
+		expect(a.data.length).toBe(5);
+		expect(a.shape).toMatchObject([1,5]);
+
+		const b = torch.rand(2, 5).toObject();
+		expect(b.data.length).toBe(10);
+		expect(b.shape).toMatchObject([2,5]);
+
+		const c = torch.rand(2, 3).toObject();
+		expect(c.data.length).toBe(6);
+		expect(c.shape).toMatchObject([2,3]);
 	})
 
 	test('Random tensor creation using shape array', () => {
-		// TODO
-		expect(true).toBe(true);
+		const a = torch.rand([1, 5]).toObject();
+		expect(a.data.length).toBe(5);
+		expect(a.shape).toMatchObject([1,5]);
+
+		const b = torch.rand([2, 5]).toObject();
+		expect(b.data.length).toBe(10);
+		expect(b.shape).toMatchObject([2,5]);
+
+		const c = torch.rand([2, 3]).toObject();
+		expect(c.data.length).toBe(6);
+		expect(c.shape).toMatchObject([2,3]);
 	})
 
 	test('Random tensor creation using option parsing', () => {
-		// TODO
-		expect(true).toBe(true);
+		const a = torch.rand([1, 5], {
+		    dtype: torch.float64
+		}).toObject();
+		expect(a.data.length).toBe(5);
+		expect(a.shape).toMatchObject([1,5]);
 	})
 
 	test('Random tensor creation using invalid params', () => {
