4ef5db0572fcc3ed498bce2a2eb322cfde8c8ca5
arition <aritionkb@gmail.com>
2020-10-04 01:47:46 -0700

    fix cudnn install

--- .github/workflows/scripts/install_cuda_windows.ps1
+++ .github/workflows/scripts/install_cuda_windows.ps1
@@ -130,4 +130,4 @@ if(Test-Path -Path $CUDNN_ZIP_LOCAL){
 
 Expand-Archive -Path $CUDNN_ZIP_LOCAL -DestinationPath $TEMP_PATH
 $CUDNN_EXPAND_LOCAL = Join-Path $TEMP_PATH "cuda"
-Copy-Item -Path "$($CUDNN_EXPAND_LOCAL)/*" -Destination $CUDA_PATH
+Copy-Item -Path "$($CUDNN_EXPAND_LOCAL)/*" -Destination $CUDA_PATH -Filter *.* -Force -recurse
